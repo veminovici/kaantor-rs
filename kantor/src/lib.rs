f1e7c552154526755cc5e306c653f910165e6ca9
Vlad Eminovici <veminovici@hotmail.com>
2023-03-05 18:52:37 +0200

    Add the util fn

--- kantor/src/lib.rs
+++ kantor/src/lib.rs
@@ -4,8 +4,34 @@
 
 mod aid;
 mod graph;
-pub mod node;
-pub mod protocol;
-pub mod proxy;
+mod node;
+mod protocol;
+mod proxy;
 
 pub use aid::*;
+pub use graph::*;
+pub use node::*;
+pub use protocol::*;
+pub use proxy::*;
+
+use actix::{dev::ToEnvelope, prelude::*};
+
+type PMsg<P> = ProtocolMsg<P>;
+type GMsg<P> = GraphMsg<ProtocolMsg<P>>;
+
+/// Add a bi-directional connection between two ndoes.
+pub async fn add_edge<A, P>(a: &mut Node<A, P>, b: &mut Node<A, P>)
+where
+    P: Send + 'static,
+    A: Actor,
+    A: Handler<PMsg<P>>,
+    A::Context: ToEnvelope<A, PMsg<P>>,
+    A: Handler<GMsg<P>>,
+    A::Context: ToEnvelope<A, GMsg<P>>,
+{
+    let pxy_a = a.as_proxy();
+    let pxy_b = b.as_proxy();
+
+    let _ = a.add_proxy(pxy_b).await;
+    let _ = b.add_proxy(pxy_a).await;
+}
